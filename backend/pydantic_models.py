041d9607403df4aaa69d3c1e1b5c771502bd0a9f
Matthew Holguin <158513570+Khraen@users.noreply.github.com>
2026-09-13 13:30:39 -0700

    Update pydantic_models.py. Update pydantic_models.py. Add Location, SafetyReport, and Itinerary response objects.

--- backend/pydantic_models.py
+++ backend/pydantic_models.py
@@ -2,8 +2,15 @@ from pydantic import BaseModel, ConfigDict, Field, EmailStr
 from typing import List, Optional
 from datetime import datetime, timedelta, timezone
 
-
 class Location(BaseModel):
+    name: str
+    latitude: float
+    longitude: float
+    
+class LocationResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    location_id: int
     name: str
     latitude: float
     longitude: float
@@ -16,6 +23,7 @@ class PostCreate(BaseModel):
 
 
 class PostResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
     post_id: int
     user_id: int
     caption: str
@@ -27,11 +35,31 @@ class PostResponse(BaseModel):
 class ItineraryCreate(BaseModel):
     name: str = Field(min_length=1, max_length=100)
 
+class ItineraryResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    itinerary_id: int
+    user_id: int
+    name: str
+    destinations: list[LocationResponse] = Field(
+        default_factory=list
+    )
+
 
 class SafetyReportCreate(BaseModel):
     report_type: str
     location: Location
     description: Optional[str] = None
+
+class SafetyReportResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    report_id: int
+    user_id: int
+    report_type: str
+    location: LocationResponse
+    description: Optional[str] = None
+    created_at: datetime
 
 
 class UserRegistration(BaseModel):
